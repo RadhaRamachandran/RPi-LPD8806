d681bb100082ae043719ee92f7ec2d0a974d8135
Amanda Zhang <amandazhang23@gmail.com>
2015-01-14 10:48:12 -0600

    add inst_mode.py

---
+++ inst_mode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from bootstrap import *
+
+#green pulse
+r,g,b = (0.0,255.0,0.0)
+step = 0.01
+level = 0.01
+dir = step
+while level >= 0.0:
+	led.fill(Color(r, g, b, level))
+	led.update()
+    if(level >= 0.99):
+		dir = -step
+		level += dir
+		#sleep(0.005)
+
+led.all_off()
