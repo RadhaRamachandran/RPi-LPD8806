69e60294b78cf12606bd9cc499165e387be8e514
Amanda Zhang <amandazhang23@gmail.com>
2015-01-14 11:36:29 -0600

    working on loops

--- inst_mode.py
+++ inst_mode.py
@@ -8,13 +8,13 @@ inst_data = np.genfromtxt('inst_data.csv',delimiter=',', usecols = 3)
 #green pulse
 r,g,b = (0.0,255.0,0.0)
 step = 0.1
-level = 0.1
 dir = step
 
 for i in inst_data:
     print i
     if i == 1:
         print "case 1"
+        level = 0.1
         while level >= 0.0:
             print "level"
             led.fill(Color(r, g, b, level))
