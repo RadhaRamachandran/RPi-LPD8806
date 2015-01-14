99b2712061b2e175cfdf99739c05d1c075e83199
Amanda Zhang <amandazhang23@gmail.com>
2015-01-14 11:47:16 -0600

    working on loops

--- inst_mode.py
+++ inst_mode.py
@@ -10,16 +10,21 @@ inst_data = np.genfromtxt('inst_data.csv',delimiter=',', usecols = 3)
 for i in inst_data:
     print i
     if i == 1:
-        level = 0.1
-        step = 0.1
-        dir = step
-        while level >= 0.1:
-            led.fill(Color(0.0, 255.0, 0.0, level))
+        print 'I\'m an energy saver!'
+        # level = 0.2
+        # step = 0.1
+        # dir = step
+        # while level >= 0.2:
+        #     led.fill(Color(0.0, 255.0, 0.0, level))
+        #     led.update()
+        #     if(level >= 0.8):
+        #         dir = -step
+        #     level += dir
+    elif i == 2:
+        anim = Rainbow(led)
+        for i in range(60):
+            anim.step()
             led.update()
-            if(level >= 0.8):
-                dir = -step
-            level += dir
-            #sleep(0.005)
     else:
         print 'else color blue'
         led.fill(Color(0.0, 0.0, 255.0, .8))
